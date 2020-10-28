03fb885611377011af25b5dd02e3620c5b0f2733
Mateusz Migas <mateuszmigas.dev@gmail.com>
2020-10-28 15:51:12 +0100

    added patching

---
+++ src/types/patch.ts
@@ -0,0 +1,54 @@
+import { hasProperty } from "../common/typeGuards";
+import { assertNever } from "../utils/typeHelpers";
+import { ValueOf } from "./common";
+
+type PatchObject<P, V> = { path: P; value: V };
+type PatchArray<P, V> =
+  | { op: "add"; path: P; values: V[] }
+  | { op: "remove"; path: P; indexes: number[] }
+  | {
+      op: "replace";
+      path: P;
+      index: number;
+      value: V;
+    };
+
+export type Patch<T> = ValueOf<
+  {
+    [P in keyof T]: T[P] extends Array<infer U>
+      ? PatchArray<P, U> | PatchObject<P, T[P]>
+      : PatchObject<P, T[P]>;
+  }
+>;
+
+export const applyPatches = <T>(object: T, patches: Patch<T>[]) => {
+  patches.forEach(patch => {
+    if (hasProperty(patch, "op")) {
+      const patchArray = (patch as unknown) as PatchArray<keyof T, unknown>;
+      const array = object[patchArray.path];
+
+      if (!Array.isArray(array))
+        throw new Error(`Cannot apply array patch '${patch.op}' to an object`);
+
+      switch (patchArray.op) {
+        case "add": {
+          array.push(patchArray.values);
+          break;
+        }
+        case "remove": {
+          patchArray.indexes.forEach(i => array.splice(i, 1));
+          break;
+        }
+        case "replace":
+          array[patchArray.index] = patchArray.value;
+          break;
+        default: {
+          assertNever(patchArray);
+        }
+      }
+    } else {
+      const patchObject = patch as PatchObject<keyof T, unknown>;
+      Object.assign(object, { [patchObject.path]: patchObject.value });
+    }
+  });
+};
