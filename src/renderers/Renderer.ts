9ad56c3ab729151971811eb3e16634a74c1dd07a
Mateusz Migas <mateuszmigas.dev@gmail.com>
2020-10-28 12:20:40 +0100

    patch

--- src/renderers/Renderer.ts
+++ src/renderers/Renderer.ts
@@ -1,22 +1,50 @@
+import { ValueOf } from "./../types/common";
 import { PickingOptions, PickingResult } from "../picking";
 import { Size, Viewport } from "../types";
 
-export type PatchObject<T> = {
-  added: { key: string; value: any }[];
-  modified: { key: string; value: any }[];
-  removed: string[];
+export type PatchRecord<K, V> = {
+  op: "add" | "remove" | "replace";
+} & (V extends Array<infer U>
+  ? {
+      path: [K, number];
+      value: U;
+    }
+  : {
+      path: K;
+      value: V;
+    });
+
+export type Patch<T> = PatchRecord<keyof T, ValueOf<T>>[];
+type Payload = {
+  name: {
+    first: string;
+    last: string;
+  };
+  ages: number[];
+  ads: { loc: string; code: number }[];
 };
 
-export type PatchArray<T> = {
-  added: T[];
-  modified: T[];
-  removed: T[];
+const patch: Patch<Payload> = [
+  { op: "replace", path: "name", value: { first: "fse", last: "fse" } },
+  { op: "replace", path: "name", value: { first: "fse", last: "fse" } },
+  { op: "replace", path: ["ages", 2], value: 2 },
+  { op: "replace", path: ["ages", 2], value: 2 },
+];
+
+export const dupa = {
+  name: "ssefse",
+  ages: [1, 3, 2],
+  ads: [
+    {
+      loc: "fsef",
+      code: 234,
+    },
+    {
+      loc: "xxx",
+      code: 333,
+    },
+  ],
 };
-
-//createPatch
-//applyPatch
-
-export type Patch<T> = {};
 
 export interface Renderer {
   render(renderPayload: unknown): void;
@@ -27,7 +55,7 @@ export interface Renderer {
   dispose(): void;
 }
 
-export interface GenericRender<T> extends Renderer {
+export interface GenericRender<T> {
   render(renderPayload: T): void;
   setSize(size: Size): void;
   setViewport(viewport: Viewport): void;
